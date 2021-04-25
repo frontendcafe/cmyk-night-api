c840941f2f54d29b7a017e1b7fdfbc993dddff3c
Nicolas Juanico <nicolas.juany@gmail.com>
2021-04-24 21:50:19 -0300

    event controller

--- src/controllers/events.controllers.ts
+++ src/controllers/events.controllers.ts
@@ -15,89 +15,31 @@ export const getEvents = async (
   _: Request,
   res: Response
 ): Promise<Response> => {
-  const response = await prisma.social_event_schedule.findMany({
-    distinct: ["schedule_id", "social_event_id", "id"],
-    where: {
-      remaining: {
-        gt: 1,
-      },
-    },
+  const response = await prisma.socialEvents.findMany({
     select: {
-      remaining: true,
-      enabled: true,
-      social_event: {
-        select: {
-          title: true,
-          description: true,
-          avatar_image: true,
-          banner_image: true,
-          rating: true,
-          type: true,
-        },
-      },
-      schedule: {
-        select: { datetime: true },
-      },
+      title: true,
+      avatar: true,
+      banner: true,
+      description: true,
+      rating: true,
     },
   });
 
   return res.status(200).json(response);
 };
 
-// export const getUserById = async (
-//   req: Request,
-//   res: Response
-// ): Promise<Response> => {
-//   const id = parseInt(req.params.id, 10);
+export const getEventById = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { id } = req.params;
+  const response = await prisma.socialEvents.findUnique({
+    where: { id: +id },
+    include: {
+      performer: true,
+      socialEventSchedule: true,
+    },
+  });
 
-//   const response = (await database
-//     .query("SELECT * FROM users WHERE id = $1", [id])
-//     .catch((e) => res.status(500).send(e.message))) as QueryResult;
-//   return res
-//     .status(200)
-//     .send({ getUserById: response.rows.length ? { ...response.rows[0] } : {} });
-// };
-
-// export const createUser = async (
-//   req: Request,
-//   res: Response
-// ): Promise<Response> => {
-//   const { email, name, age, created_at, updated_at } = req.body;
-//   (await database
-//     .query(
-//       "INSERT INTO users (email, name, age, created_at, updated_at) VALUES ($1,$2,$3,$4,$5)",
-//       [email, name, age, created_at, updated_at]
-//     )
-//     .catch((e) => res.status(500).send(e.message))) as QueryResult;
-//   return res.status(200).send({
-//     createUser: { email, name, age, created_at, updated_at },
-//     message: "User created successfuly",
-//   });
-// };
-
-// export const updateUser = async (
-//   req: Request,
-//   res: Response
-// ): Promise<Response> => {
-//   const id = parseInt(req.params.id, 10);
-//   const { name } = req.body;
-//   const response = (await database
-//     .query("UPDATE users SET  name = $2 WHERE id = $1", [id, name])
-//     .catch((e) => res.status(500).send(e.message))) as QueryResult;
-//   return res
-//     .status(200)
-//     .send({ message: `User ${id} has been sucessfully updated.` });
-// };
-
-// export const deleteUser = async (
-//   req: Request,
-//   res: Response
-// ): Promise<Response> => {
-//   const id = parseInt(req.params.id, 10);
-//   (await database
-//     .query("DELETE FROM users WHERE id = $1", [id])
-//     .catch((e) => res.status(500).send(e.message))) as QueryResult;
-//   return res
-//     .status(200)
-//     .send({ message: `User ${id} has been sucessfully deleted.` });
-// };
+  return res.status(200).json(response);
+};
