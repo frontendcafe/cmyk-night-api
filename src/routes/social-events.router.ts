4a276cee3c68296350febe13565ac91ed5899f94
Nicolas Juanico <nicolas.juany@gmail.com>
2021-04-25 22:34:25 -0300

    validate params

--- src/routes/social-events.router.ts
+++ src/routes/social-events.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getEventById, getEvents } from "../controllers/events.controllers";
+import { validateId } from "../middleware/validate-id.middleware";
 
 export const eventsRouter = Router();
 
 eventsRouter.get("/test", (_, res) => res.status(200).send("123"));
 
 eventsRouter.get("", getEvents);
-eventsRouter.get("/:id", getEventById);
+eventsRouter.get("/:id", validateId, getEventById);
